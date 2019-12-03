676de4dad2ca13a86a8d76cc2ae96572e87d9c8a
alaposi <agnes.laposi@gmail.com>
2019-12-03 19:45:59 +0100

    repository for reporter list is added

---
+++ src/main/java/com/alaposi/notebookreport/repositories/ReporterRepository2.java
@@ -0,0 +1,11 @@
+package com.alaposi.notebookreport.repositories;
+
+import com.alaposi.notebookreport.models.Reporter2;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReporterRepository2 extends CrudRepository<Reporter2, Long> {
+
+}
+
